4c9026a46664eab8e5b8165c769f03a0af32980d
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2024-03-02 14:21:34 +0700

    set loading state

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,11 +3,12 @@ import { useState, useEffect } from 'react';
 import { IconCirclePlus } from '@tabler/icons-react';
 
 import { useDisclosure } from '@mantine/hooks';
-import { AppShell, Text, Stack, rem, Divider, Modal, Center, UnstyledButton, SimpleGrid, TextInput, Group, Button, NativeSelect } from '@mantine/core';
+import { AppShell, Text, Stack, rem, Divider, Modal, Center, UnstyledButton, SimpleGrid, TextInput, Group, Button, NativeSelect, Loader } from '@mantine/core';
 import StoryCardAdmin from '../components/StoryCardAdmin';
 
 export default function DashboardPage() {
 
+    const [loading, setLoading] = useState(false)
     const [stories, setStories] = useState([])
     const [name, setName] = useState(() => {
         return localStorage.getItem('username')
@@ -16,20 +17,22 @@ export default function DashboardPage() {
     const [category, setCategory] = useState('Anime')
     const [RqEp, setRqEp] = useState([])
 
-    const urlUserStory = `http://localhost:3000/stories/owner/${name}`
-    const urlNewStory = "http://localhost:3000/stories"
-    const urltagRequest = `http://localhost:3000/rqtags/board`
+    const mainurl = 'https://pj-backend.up.railway.app'
+    const urlUserStory = `${mainurl}/stories/owner/${name}`
+    const urlNewStory = `${mainurl}/stories`
+    const urltagRequest = `${mainurl}/rqtags/board`
 
 
 
     useEffect(() => {
+        setLoading(true)
         const fetchData = async () => {
 
             await fetch(urlUserStory, {
                 method: "GET"
             })
                 .then(response => response.json())
-                .then(result => setStories(result))
+                .then(result => {setStories(result); setLoading(false); })
                 .catch(e => console.log(e))
         }
         fetchData()
@@ -49,6 +52,7 @@ export default function DashboardPage() {
     }, [])
 
     const onSubmit = () => {
+        setLoading(true)
         const payload = {
             storyname,
             category,
@@ -62,16 +66,17 @@ export default function DashboardPage() {
         };
         fetch(urlNewStory, requestOptions)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => { console.log(data); setLoading(false); })
+            .catch(e => console.log(e))
     }
 
 
     const items = stories.map((item) => (
-        
-        <StoryCardAdmin stid={item['_id']}title={item['storyname']} 
-        category={item['category']} description={item['description']} 
-        Ep={item['episodeId']} image={item['image']} 
-        rq={RqEp.find((element) => element == item['_id']) != undefined }></StoryCardAdmin>
+
+        <StoryCardAdmin stid={item['_id']} title={item['storyname']}
+            category={item['category']} description={item['description']}
+            Ep={item['episodeId']} image={item['image']}
+            rq={RqEp.find((element) => element == item['_id']) != undefined}></StoryCardAdmin>
 
     ));
 
@@ -83,53 +88,54 @@ export default function DashboardPage() {
 
     return (
         <AppShell.Main>
-            <Stack
-                h={300}
-                bg="var(--mantine-color-body)"
-                px={rem(100)}
-            >
-                <Text fw={500} py={rem(30)} size={rem(40)}  color='black' >
-                    DASHBOARD
-                </Text>
-                <Text fw={200} size="md"  color='black' py={rem(0)}>
-                    you have {stories.length} boards
-                </Text>
-                <Divider my="md" />
-                <SimpleGrid cols={4}>
-                    {items}
-                    <Modal opened={opened} onClose={close} title="Create new board" centered>
-                        <form onSubmit={onSubmit}>
-                            <TextInput
-                                withAsterisk
-                                label="Board title"
-                                placeholder="your board title"
-                                onChange={e => setStoryname(e.target.value)}
-                            />
-                            <NativeSelect
-                                label="Category"
-                                data={['Anime', 'TV Series', 'Online Program', 'Podcast']}
-                                onChange={e => setCategory(e.target.value)}
-                            />
+            {loading ? <Loader color="blue" size="xl" /> :
+                <Stack
+                    h={300}
+                    bg="var(--mantine-color-body)"
+                    px={rem(100)}
+                >
+                    <Text fw={500} py={rem(30)} size={rem(40)} color='black' >
+                        DASHBOARD
+                    </Text>
+                    <Text fw={200} size="md" color='black' py={rem(0)}>
+                        you have {stories.length} boards
+                    </Text>
+                    <Divider my="md" />
+                    <SimpleGrid cols={4}>
+                        {items}
+                        <Modal opened={opened} onClose={close} title="Create new board" centered>
+                            <form onSubmit={onSubmit}>
+                                <TextInput
+                                    withAsterisk
+                                    label="Board title"
+                                    placeholder="your board title"
+                                    onChange={e => setStoryname(e.target.value)}
+                                />
+                                <NativeSelect
+                                    label="Category"
+                                    data={['Anime', 'TV Series', 'Online Program', 'Podcast']}
+                                    onChange={e => setCategory(e.target.value)}
+                                />
 
-                            <Group justify="flex-end" mt="md">
-                                <Button type="submit" onClick={close} color="#2CB5B5">Submit</Button>
-                                <Button type="reset" variant="outline" onClick={()=>setStoryname('')} color="#FF6666">Cancle</Button>
-                            </Group>
-                        </form>
-                    </Modal>
-                    <UnstyledButton
-                        onClick={open}
-                    >
-                        <Center>
-                            {Newicon}
-                        </Center>
+                                <Group justify="flex-end" mt="md">
+                                    <Button type="submit" color="#2CB5B5">Submit</Button>
+                                    <Button type="reset" variant="outline" onClick={() => setStoryname('')} color="#FF6666">Cancle</Button>
+                                </Group>
+                            </form>
+                        </Modal>
+                        <UnstyledButton
+                            onClick={open}
+                        >
+                            <Center>
+                                {Newicon}
+                            </Center>
 
-                    </UnstyledButton>
+                        </UnstyledButton>
 
-                </SimpleGrid>
+                    </SimpleGrid>
 
 
-            </Stack>
+                </Stack>}
         </AppShell.Main >);
 
 }
